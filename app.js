0e6fbca0429bb67500dd4e6c05bf170fa0649845
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-06-18 15:01:23 +0100

    set up mongodb and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+require("dotenv").config();
 const apiRouter = require("./routes/api");
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 app.use("/api", apiRouter);
 
