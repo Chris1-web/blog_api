ed17d764bb86df3cda753460bd8a2a1b82ec0657
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-06-25 20:04:54 +0100

    upadte user model fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,29 +2,60 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const user = require("../models/user");
+
+function verifyToken(req, res, next) {
+  // check if header has authorization with correct token
+  // get token from header
+  const { authorization } = req.headers;
+  if (authorization !== undefined) {
+    const bearerToken = authorization.split(" ")[1];
+    jwt.verify(bearerToken, process.env.JWT_SECRET, (err, userData) => {
+      if (err) {
+        res.status(403).json({ err });
+      } else {
+        // store user in req.user to be accessible to route
+        const user = {
+          _id: userData._id,
+          username: userData.username,
+          blog: userData.blog,
+        };
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    res.status(403).json({ message: "Forbidden" });
+  }
+}
 
 // display all users***
-exports.user_get = async (req, res) => {
-  const { page } = req.query;
-  const options = {
-    select: "username",
-    page: parseInt(page, 10) || 1,
-    limit: 3,
-  };
+exports.user_get = [
+  verifyToken,
+  async (req, res) => {
+    const { page } = req.query;
+    const options = {
+      select: "username blog",
+      page: parseInt(page, 10) || 1,
+      limit: 3,
+    };
 
-  User.paginate({}, options).then((results, error) => {
-    if (error) {
-      res.status(500).json({ error });
-    }
-    // pass totalPages number along with the results
-    res.status(200).json({
-      users: results.docs,
-      page_count: results.totalPages,
-      current_page: results.page,
+    User.paginate({}, options).then((results, error) => {
+      if (error) {
+        res.status(500).json({ error });
+      }
+      // pass totalPages number along with the results
+      res.status(200).json({
+        users: results.docs,
+        page_count: results.totalPages,
+        current_page: results.page,
+        activeUser: req.user,
+      });
     });
-  });
-};
+  },
+];
 
+// create user account
 exports.user_post = [
   body("username", "username is required")
     .trim()
@@ -37,8 +68,9 @@ exports.user_post = [
       return value;
     }),
   body("password", "password is required").trim().isLength({ min: 1 }).escape(),
+  body("blog", "blog type is required").trim().isLength({ min: 1 }).escape(),
   (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, blog } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({
@@ -52,6 +84,7 @@ exports.user_post = [
         const user = new User({
           username,
           password: hashedPassword,
+          blog,
         });
         await user.save();
         res.json({ user, message: "User Created Successfully" });
@@ -62,6 +95,7 @@ exports.user_post = [
   },
 ];
 
+// login user to generate token
 exports.user_login = [
   body("username", "username is required").trim().isLength({ min: 1 }).escape(),
   body("password", "password is required").trim().isLength({ min: 1 }).escape(),
--- models/user.js
+++ models/user.js
@@ -5,6 +5,12 @@ const mongoosePaginate = require("mongoose-paginate-v2");
 const UserSchema = new Schema({
   username: { type: String, required: true, minLength: 3, maxLength: 100 },
   password: { type: String, required: true, minLength: 3, maxLength: 100 },
+  blog: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+  },
 });
 
 UserSchema.plugin(mongoosePaginate);
