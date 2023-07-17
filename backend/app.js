2800317bfec86a827a6fb6bf617d582afcb0f37e
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-07-17 03:40:35 +0100

    add signup request to backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
 require("dotenv").config();
@@ -14,6 +15,7 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/src/app/sign-up/page.js
+++ frontend/src/app/sign-up/page.js
@@ -1,20 +1,75 @@
+"use client";
+import React, { useState } from "react";
+import style from "./page.module.css";
+
 export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [blog, setBlog] = useState("");
+  const [password, setPassword] = useState("");
+
+  function changeUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  function changeBlog(e) {
+    setBlog(e.target.value);
+  }
+
+  function changePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  async function Signup(e) {
+    e.preventDefault();
+    const data = { username, blog, password };
+    try {
+      const response = await fetch("http://localhost:3001/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+    console.log(data);
+  }
+
   return (
-    <div>
+    <div className={style.sign_up}>
       <h1>Sign Up</h1>
-      <form>
-        <div className="username">
+      <form className={style.form} onSubmit={Signup}>
+        <div>
           <label>Username</label>
-          <input type="text" />
+          <input
+            className={style.input}
+            type="text"
+            value={username}
+            onChange={changeUsername}
+          />
         </div>
-        <div className="blog_type">
+        <div>
           <label>Genre</label>
-          <input type="text" />
+          <input
+            className={style.input}
+            type="text"
+            value={blog}
+            onChange={changeBlog}
+          />
         </div>
-        <div className="username">
+        <div>
           <label>Password</label>
-          <input type="password" />
+          <input
+            className={style.input}
+            type="password"
+            value={password}
+            onChange={changePassword}
+          />
         </div>
+        <button>Sign Up</button>
       </form>
     </div>
   );
