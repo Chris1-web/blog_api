51b74f6177ecded884a604ad498ea514eca1e311
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-07-23 14:10:05 +0100

    make navbar communicate with server to display appropriate content

--- frontend/src/app/components/Nav/Nav.js
+++ frontend/src/app/components/Nav/Nav.js
@@ -7,6 +7,7 @@ import { toast } from "react-hot-toast";
 export default function Nav() {
   const [showProfile, setShowProfile] = useState(false);
   const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
   async function getUser(token) {
     try {
       const response = await fetch("http://localhost:3001/api/user", {
@@ -20,6 +21,8 @@ export default function Nav() {
       if (!response.ok) {
         localStorage.removeItem("token");
         const text = await response.json();
+        setLoading(false);
+        showProfile(false);
         throw new Error(JSON.stringify(text));
       }
 
@@ -27,6 +30,7 @@ export default function Nav() {
       const result = await response.json();
       const { activeUser } = result;
       setUserData({ ...activeUser });
+      setLoading(false);
       setShowProfile(true);
     } catch (error) {
       setShowProfile(false);
@@ -39,29 +43,66 @@ export default function Nav() {
       : null;
     if (token) {
       getUser(token);
+    } else {
+      setShowProfile(false);
+      setLoading(false);
     }
   }, []);
-
-  return !showProfile ? (
-    <nav className={styles.nav}>
-      <Link href="/">BLOGGERS</Link>
-      <ul>
-        <li>
-          <Link href="/login">Sign In</Link>
-        </li>
-        <li>
-          <Link href="/sign-up">Sign Up</Link>
-        </li>
-      </ul>
-    </nav>
-  ) : (
-    <nav className={styles.nav}>
-      <Link href="/">BLOGGERS</Link>
-      <ul>
-        <li className={styles.userProfile}>
-          <Link href={"/user" + "/" + userData._id}>{userData.username}</Link>
-        </li>
-      </ul>
-    </nav>
+  return (
+    <>
+      {loading && (
+        <nav className={styles.nav}>
+          <Link href="/">BLOGGERS</Link>
+          <p>Loading...</p>
+        </nav>
+      )}
+      {!loading && !showProfile && (
+        <nav className={styles.nav}>
+          <Link href="/">BLOGGERS</Link>
+          <ul>
+            <li>
+              <Link href="/login">Sign In</Link>
+            </li>
+            <li>
+              <Link href="/sign-up">Sign Up</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+      {!loading && showProfile && (
+        <nav className={styles.nav}>
+          <Link href="/">BLOGGERS</Link>
+          <ul>
+            <li className={styles.userProfile}>
+              <Link href={"/user" + "/" + userData._id}>
+                {userData.username}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
   );
+  // return !showProfile ? (
+  //   <nav className={styles.nav}>
+  //     <Link href="/">BLOGGERS</Link>
+  //     <ul>
+  //       <li>
+  //         <Link href="/login">Sign In</Link>
+  //       </li>
+  //       <li>
+  //         <Link href="/sign-up">Sign Up</Link>
+  //       </li>
+  //     </ul>
+  //   </nav>
+  // ) : (
+  //   <nav className={styles.nav}>
+  //     <Link href="/">BLOGGERS</Link>
+  //     <ul>
+  //       <li className={styles.userProfile}>
+  //         <Link href={"/user" + "/" + userData._id}>{userData.username}</Link>
+  //       </li>
+  //     </ul>
+  //   </nav>
+  // );
 }
